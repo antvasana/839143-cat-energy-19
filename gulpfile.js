b219e22f725a599e5b32be49218382e01a640135
Tatiana <tgurieva04@gmail.com>
2020-05-14 09:47:32 -0300

    продолжает автоматизацию проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 "use strict";
 
 var gulp = require("gulp");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -17,9 +21,28 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png, jpg, svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.mozjpeg({ progressive: true }),
+      imagemin.svgo()
+
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png, jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
